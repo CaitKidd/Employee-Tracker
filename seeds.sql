2349bb0a4d42a0d6d831614816250a25baf05f75
Caitlyn Kidd <Caitlyn.Kidd92@gmail.com>
2022-03-17 12:32:44 -0600

    .

---
+++ seeds.sql
@@ -0,0 +1,9 @@
+
+use employees_DB;
+
+insert into employee (first_name, last_name)
+values ("Swanson", "Ron"), ("April", "Ludgate"), ("Andy", "Dwyer"), ("Donna", "Meagle"), ("Tom", "Haverford"), ("Ben", "Wyatt");
+insert into role (title, salary)
+values ("Manager", 90000), ("Accountant", 75000), ("Sales Lead", 60000), ("Office Manager", 55000), ("Salesperson", 40000), ("Legal Team", 50000);
+insert into department (names)
+values ("Management"), ("Accountant"), ("Sales"), ("Legal");
