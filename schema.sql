7f1143861a93ffc60af2a793477675fdfa415bea
caitlyn kidd <caitlyn.kidd92@gmail.com>
2022-03-19 00:48:30 -0600

    .

--- schema.sql
+++ schema.sql
@@ -14,4 +14,13 @@ dept_id INTEGER NOT NULL,
 FOREIGN KEY (dept_id) REFERENCES departments(id)
 );
 
+CREATE TABLE employees (
+id INTEGER AUTO_INCREMENT NOT NULL,
+PRIMARY KEY (id),
+first_name VARCHAR(30),
+last_name VARCHAR(30),
+role_id INTEGER NOT NULL,
+FOREIGN KEY (role_id) REFERENCES roles(id),
+manager_id INTEGER,
+FOREIGN KEY (manager_id) REFERENCES employees(id)
 );
